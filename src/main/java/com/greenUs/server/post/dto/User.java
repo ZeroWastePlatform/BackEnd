29e654be33110abed8e02cd88eb9d3d986aa7cb5
한상우 <sktkddn777@naver.com>
2022-12-22 01:02:19 +0900

    Update: 상단 패키지 domain -> post 로 변경

---
+++ src/main/java/com/greenUs/server/post/dto/User.java
@@ -0,0 +1,4 @@
+Unstaged changes after reset:
+D	src/main/java/com/greenUs/server/domain/post/api/SwaggerTestController.java
+D	src/main/java/com/greenUs/server/domain/post/dto/User.java
+M	src/main/java/com/greenUs/server/post/dto/User.java
