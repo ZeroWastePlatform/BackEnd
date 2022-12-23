1fea86713924409db1f7a0c573bb06110cdcbd39
starwook <star.wookk@gmail.com>
2022-12-23 19:24:54 +0900

    feat: Category 엔티티 추가

---
+++ src/main/java/com/greenUs/server/product/domain/Category.java
@@ -0,0 +1,20 @@
+package com.greenUs.server.product.domain;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Long id;
+    private String name;
+
+    @OneToOne
+    @JoinColumn(name ="product_id")
+    private Product product;
+}
