3ab2df735f4d1efbab0eb3dccf73bd60c1b79011
starwook <star.wookk@gmail.com>
2022-12-23 19:24:09 +0900

    feat: PurcahseProduct 레포지토리 추가

--- src/main/java/com/greenUs/server/purchase/repository/PurchaseProductRepository.java
+++ src/main/java/com/greenUs/server/purchase/repository/PurchaseProductRepository.java
@@ -1,10 +1,10 @@
 package com.greenUs.server.purchase.repository;
 
-import com.greenUs.server.purchase.domain.Purchase;
+
+import com.greenUs.server.purchase.domain.PurchaseProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
-public interface PurchaseRepository extends JpaRepository<Purchase, Long> {
+public interface PurchaseProductRepository extends JpaRepository<PurchaseProduct,Long> {
 }
