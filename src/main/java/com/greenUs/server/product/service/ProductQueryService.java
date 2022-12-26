e94a2418cf4e3eb12c7b82577eade7b3c1a53345
starwook <star.wookk@gmail.com>
2022-12-27 05:14:12 +0900

    feat: Product->ProductForm 메소드, 전체 조회,카테고리 별 조회 메소드 추가

--- src/main/java/com/greenUs/server/product/service/ProductQueryService.java
+++ src/main/java/com/greenUs/server/product/service/ProductQueryService.java
@@ -1,12 +1,48 @@
 package com.greenUs.server.product.service;
 
 
+import com.greenUs.server.product.domain.Product;
+import com.greenUs.server.product.dto.ProductForm;
 import com.greenUs.server.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ProductQueryService {
+    //Mapper 보류(논의) 그냥 builder()?
     private final ProductRepository productRepository;
+
+    @Transactional
+    public Page<ProductForm> getHomeProducts(Pageable pageable){
+        Page<Product> products = productRepository.findAll(pageable);
+        Page<ProductForm> productForms = products.map(product -> makeProductForm(product));
+        return productForms;
+    }
+
+    @Transactional
+    public Page<ProductForm> getProductByCategory(String categoryName,Pageable pageable){
+        Page<Product> products = productRepository.findAllByCategory(categoryName,pageable);
+        Page<ProductForm> productForms = products.map(product -> makeProductForm(product));
+        return productForms;
+    }
+
+    private ProductForm makeProductForm(Product product) {
+        ProductForm productForm = ProductForm.builder()
+                .id(product.getId())
+                .image(product.getImage())
+                .brand(product.getBrand())
+                .price(product.getPrice())
+                .title(product.getTitle())
+                .build();
+        return productForm;
+    }
 }
