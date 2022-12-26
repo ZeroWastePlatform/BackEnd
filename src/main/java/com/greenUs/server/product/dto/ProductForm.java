db99c2884e9636de469f1ca6b52cf2485ac33c09
starwook <star.wookk@gmail.com>
2022-12-27 05:10:41 +0900

    refactor: 변수 소문자

--- src/main/java/com/greenUs/server/product/dto/ProductForm.java
+++ src/main/java/com/greenUs/server/product/dto/ProductForm.java
@@ -1,12 +1,14 @@
 package com.greenUs.server.product.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 
 @Builder
+@AllArgsConstructor
 public class ProductForm {
     private Long id;
     private String title;
-    private String Brand;
+    private String brand;
     private String image;
     private int price;
 }
