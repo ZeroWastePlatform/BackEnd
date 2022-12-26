f6f1dbae034e7d0cc3e06d4097ca31dfd5be19a1
starwook <star.wookk@gmail.com>
2022-12-27 05:11:33 +0900

    feat: 홈화면 인기상품, 카테고리별 상품 메소드 추가

--- src/main/java/com/greenUs/server/product/controller/ProductQueryController.java
+++ src/main/java/com/greenUs/server/product/controller/ProductQueryController.java
@@ -1,4 +1,40 @@
 package com.greenUs.server.product.controller;
 
+
+import com.greenUs.server.product.domain.Product;
+import com.greenUs.server.product.dto.ProductForm;
+import com.greenUs.server.product.service.ProductQueryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
 public class ProductQueryController {
+
+    private static final int DEFAULT_PAGE_SIZE =10;
+    private static final String DEFAULT_PAGE_SORT = "viewCount";
+    private final ProductQueryService productQueryService;
+
+    @GetMapping("/products")
+    public Page<ProductForm> getHomeProducts(
+            @PageableDefault(size = DEFAULT_PAGE_SIZE,sort = DEFAULT_PAGE_SORT,direction = Sort.Direction.DESC) Pageable pageable){
+        return productQueryService.getHomeProducts(pageable);
+    }
+
+    @GetMapping("/products/{categoryName}")
+    public Page<ProductForm> getProductByCategory(
+            @PathVariable("categoryName") String categoryName,
+            @PageableDefault(size = DEFAULT_PAGE_SIZE,sort = DEFAULT_PAGE_SORT,direction = Sort.Direction.DESC) Pageable pageable){
+        return productQueryService.getProductByCategory(categoryName,pageable);
+    }
+
+
 }
