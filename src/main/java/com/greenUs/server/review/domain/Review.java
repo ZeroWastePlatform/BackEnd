9a671439fd7c3286675118bafa45b353a1fb72a2
starwook <star.wookk@gmail.com>
2023-01-14 06:06:29 +0900

    feat: 리뷰 엔티티 추가

---
+++ src/main/java/com/greenUs/server/review/domain/Review.java
@@ -0,0 +1,37 @@
+package com.greenUs.server.review.domain;
+
+import com.greenUs.server.common.BaseEntity;
+import com.greenUs.server.member.domain.Member;
+import com.greenUs.server.post.dto.User;
+import com.greenUs.server.product.domain.Product;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review extends BaseEntity {
+
+    @Id
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name ="product_id")
+    private Product product;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private String description;
+    private String image;
+    private int starCnt;
+    private int helpCnt;
+
+}
