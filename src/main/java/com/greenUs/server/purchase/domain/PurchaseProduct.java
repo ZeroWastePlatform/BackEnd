4a11af56fe61abf6f809ebd3d0ad34868f3685fc
한상우 <sktkddn777@naver.com>
2023-02-11 18:32:30 +0900

    Update: Purchase, PurchaseProduct domain
    toString 어노테이션 추가
    Purchase 엔티티는 delivery 이름 지정
    생성자 임의로 생성 (추후 수정)

--- src/main/java/com/greenUs/server/purchase/domain/PurchaseProduct.java
+++ src/main/java/com/greenUs/server/purchase/domain/PurchaseProduct.java
@@ -2,16 +2,17 @@ package com.greenUs.server.purchase.domain;
 
 import com.greenUs.server.common.BaseEntity;
 import com.greenUs.server.product.domain.Product;
-import com.greenUs.server.purchase.domain.Purchase;
+import lombok.ToString;
 
 import javax.persistence.*;
 
+@ToString
 @Entity
 public class PurchaseProduct extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "Purchase_product_id")
+    @Column(name = "purchase_product_id")
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
