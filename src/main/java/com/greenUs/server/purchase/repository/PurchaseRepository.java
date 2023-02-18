5f3876063eec6aa3e3d796c5c0978ecb0444c925
한상우 <sktkddn777@naver.com>
2023-02-18 13:16:39 +0900

    Add: Purchase Repository
    findByMemberId 추가

--- src/main/java/com/greenUs/server/purchase/repository/PurchaseRepository.java
+++ src/main/java/com/greenUs/server/purchase/repository/PurchaseRepository.java
@@ -1,10 +1,19 @@
 package com.greenUs.server.purchase.repository;
 
 import com.greenUs.server.purchase.domain.Purchase;
+import com.greenUs.server.purchase.dto.response.PurchaseResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
 
 
 @Repository
 public interface PurchaseRepository extends JpaRepository<Purchase, Long> {
+
+    @Query("select p from Purchase p\n"
+        + "where p.member.id = :id")
+    Page<PurchaseResponse> findByMemberId(Long id, Pageable pageable);
 }
