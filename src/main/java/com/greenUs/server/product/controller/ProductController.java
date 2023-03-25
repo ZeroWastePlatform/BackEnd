694d37795aad86820dc444739e8a971190498b1a
한상우 <sktkddn777@naver.com>
2023-03-26 02:49:54 +0900

    Docs: product like 스웨거 작성

--- src/main/java/com/greenUs/server/product/controller/ProductController.java
+++ src/main/java/com/greenUs/server/product/controller/ProductController.java
@@ -6,6 +6,7 @@ import com.greenUs.server.product.domain.Category;
 import com.greenUs.server.product.dto.request.ProductsRequest;
 import com.greenUs.server.product.dto.response.ProductDetailResponse;
 import com.greenUs.server.product.dto.response.ProductsResponse;
+import com.greenUs.server.product.exception.LikeDuplicateException;
 import com.greenUs.server.product.exception.NotFoundProductException;
 import com.greenUs.server.product.service.ProductService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -58,6 +59,12 @@ public class ProductController {
         }
     }
 
+    @Operation(summary = "상품 좋아요", description = "상품 좋아요")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "상품 좋아요 성공"),
+            @ApiResponse(responseCode = "400", description = "상품 번호가 DB 에 없음", content = @Content(schema = @Schema(implementation = NotFoundProductException.class))),
+            @ApiResponse(responseCode = "409", description = "좋아요가 중복되었음", content = @Content(schema = @Schema(implementation = LikeDuplicateException.class)))
+    })
     @PostMapping("/{id}/like")
     public ResponseEntity<Void> likeProduct(
             @Parameter(description = "accessToken 값", in = ParameterIn.HEADER) @AuthenticationPrincipal LoginMember loginMember,
@@ -68,6 +75,12 @@ public class ProductController {
         }
     }
 
+    @Operation(summary = "상품 좋아요 취소", description = "상품 좋아요 취소")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "상품 좋아요 취소 성공"),
+            @ApiResponse(responseCode = "400", description = "상품 번호가 DB 에 없음", content = @Content(schema = @Schema(implementation = NotFoundProductException.class))),
+            @ApiResponse(responseCode = "409", description = "좋아요가 중복되었음", content = @Content(schema = @Schema(implementation = LikeDuplicateException.class)))
+    })
     @DeleteMapping("/{id}/like")
     public ResponseEntity<Void> likeCancelProduct(
             @Parameter(description = "accessToken 값", in = ParameterIn.HEADER) @AuthenticationPrincipal LoginMember loginMember,
